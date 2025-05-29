56d7e13f423db53b6defd126d382a2a988fda61e
ziaurehmanii <ziaurrehmanrehmani@gmail.com>
2025-05-29 13:32:01 -0700

    Add CMake installation script and compile commands for c_Https_Server
    - Created cmake_install.cmake to define installation parameters and manage installation manifest.
    - Generated compile_commands.json to provide build commands for source files, including main.cpp and hdlibc.cpp.
    - Set default installation prefix and configuration name, ensuring compatibility with cross-compilation.

---
+++ build/CMakeFiles/c_https_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_https_server.dir/main.cpp.o"
+  "CMakeFiles/c_https_server.dir/main.cpp.o.d"
+  "CMakeFiles/c_https_server.dir/src/hdlibc.cpp.o"
+  "CMakeFiles/c_https_server.dir/src/hdlibc.cpp.o.d"
+  "bin/c_https_server"
+  "bin/c_https_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_https_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/c_https_server.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for c_https_server.
+# This may be replaced when dependencies are built.
